0cc2f9db056e44366743701250a5a3f696deec7e
Omar Salloum <proneoas@gmail.com>
2012-08-22 17:53:24 +0300

    Logging on Debug
    Add Logging function that outputs pretty on debug

--- MyTV.IOS.Framework/MyTV.IOS/Logging.h
+++ MyTV.IOS.Framework/MyTV.IOS/Logging.h
@@ -8,6 +8,15 @@
 
 #import <Foundation/Foundation.h>
 
+
+#ifdef DEBUG
+#   define DLog(__FORMAT__, ...) NSLog((@"%s [Line %d] " __FORMAT__), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#else
+#   define DLog(...) do {} while (0)
+#endif
+// ALog always displays output regardless of the DEBUG setting
+#define ALog(__FORMAT__, ...) NSLog((@"%s [Line %d] " __FORMAT__), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+
 @protocol Logging <NSObject>
 
 @end
