9072c4aa70370ad35a6301f473c697aed20a5a05
Omar Salloum <proneoas@gmail.com>
2012-08-28 11:05:26 +0300

    RestService::SendLinkingRequest codeblock
    Implement SendLinkingRequest using code block and amend error passing

--- MyTV.IOS.Framework/MyTV.IOS/RestService.h
+++ MyTV.IOS.Framework/MyTV.IOS/RestService.h
@@ -9,10 +9,14 @@
 #import <Foundation/Foundation.h>
 #import "Logging.h"
 #import "DataFetcher.h"
+#import "Linking.h"
+
+typedef void (^RSLinkingCallBack) (Linking*, NSError*);
 
 @interface RestService : NSObject
 
 +(void)SendLinkingRequest:(NSString *)baseUrl withDeviceId:(NSString *)deviceId andDeviceTypeId:(NSString *)deviceTypeId andCallback:(NSObject *)callbackObject usingSelector:(SEL)callbackSelector;
 
++(void)SendLinkingRequest:(NSString *)baseUrl withDeviceId:(NSString *)deviceId andDeviceTypeId:(NSString *)deviceTypeId usingCallback:(RSLinkingCallBack)callback;
 
 @end
--- MyTV.IOS.Framework/MyTV.IOS/RestService.m
+++ MyTV.IOS.Framework/MyTV.IOS/RestService.m
@@ -9,7 +9,6 @@
 #import "RestService.h"
 #import "TBXML.h"
 #import "ScanHelper.h"
-#import "Linking.h"
 
 @implementation RestService
 
@@ -33,7 +32,9 @@
                 TBXMLElement *root = document.rootXMLElement;
                 NSString *status = [TBXML textForElement:[TBXML childElementNamed:@"status" parentElement:root]];
                 if ([status compare:@"failure"] == NSOrderedSame) {
-                    [callbackObject performSelector:callbackSelector withObject:NULL withObject:NULL];
+                    error = [NSError errorWithDomain:NSPOSIXErrorDomain code:[@"1" integerValue] userInfo:@{NSLocalizedDescriptionKey: @"Webservice Failure"}];
+                    
+                    [callbackObject performSelector:callbackSelector withObject:NULL withObject:error];
                 }
                 else
                 {
@@ -65,5 +66,57 @@
     
 }
 
++(void)SendLinkingRequest:(NSString *)baseUrl withDeviceId:(NSString *)deviceId andDeviceTypeId:(NSString *)deviceTypeId usingCallback:(RSLinkingCallBack)callback {
+    
+    NSString* linkingRequestUrl = [baseUrl stringByAppendingString:[NSString stringWithFormat:@"action=linking&deviceid=%@&devicetypeid=%@", deviceId, deviceTypeId]];
+    
+    [DataFetcher Get:linkingRequestUrl usingCallback:^(NSData *data, NSError *error) {
+        DLog(@"Processing Data inside Code Block");
+        if (error != NULL) {
+            callback(NULL, error);
+        }
+        else {
+            NSError *error;
+            //GDataXMLDocument *document = [[GDataXMLDocument alloc] initWithData:data options:0 error:&error];
+            TBXML *document = [TBXML newTBXMLWithXMLData:data error:&error];
+            if(error != NULL) {
+                callback(NULL, error);
+            }
+            else {
+                TBXMLElement *root = document.rootXMLElement;
+                NSString *status = [TBXML textForElement:[TBXML childElementNamed:@"status" parentElement:root]];
+                if ([status compare:@"failure"] == NSOrderedSame) {
+                    error = [NSError errorWithDomain:NSPOSIXErrorDomain code:[@"1" integerValue] userInfo:@{NSLocalizedDescriptionKey: @"Webservice Failure"}];
+                    callback(NULL, error);
+                }
+                else
+                {
+                    Linking *object = [Linking new];
+                    TBXMLElement *root = document.rootXMLElement;
+                    TBXMLElement *customer = [TBXML childElementNamed:@"customer" parentElement:root];
+                    if (customer == NULL) {
+                        callback(NULL, NULL);
+                    }
+                    else {
+                        TBXMLElement *pin = [TBXML childElementNamed:@"PinCode" parentElement:customer];
+                        TBXMLElement *customerid = [TBXML childElementNamed:@"Id" parentElement:customer];
+                        TBXMLElement *billingid = [TBXML childElementNamed:@"BillingId" parentElement:customer];
+                        if(pin != NULL)
+                            object.PinCode = [ScanHelper getIntFromNSString:[TBXML textForElement:pin]];
+                        if(customerid != NULL)
+                            object.CustomerId = [ScanHelper getIntFromNSString:[TBXML textForElement:customerid]];
+                        if(billingid != NULL)
+                            object.BillingId = [ScanHelper getIntFromNSString:[TBXML textForElement:billingid]];
+                        callback(object, NULL);
+                        
+                    }
+                    
+                }
+                
+            }
+        }
+    }];
+    
+}
 
 @end
