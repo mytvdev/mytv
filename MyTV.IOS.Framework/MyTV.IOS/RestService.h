a495ec68b5b15836082766ee2a8b235bd1d16310
Omar Salloum <proneoas@gmail.com>
2012-08-28 16:52:41 +0300

    RestService::GetVODUrl
    GetVODUrl implementation

--- MyTV.IOS.Framework/MyTV.IOS/RestService.h
+++ MyTV.IOS.Framework/MyTV.IOS/RestService.h
@@ -12,11 +12,14 @@
 #import "Linking.h"
 
 typedef void (^RSLinkingCallBack) (Linking*, NSError*);
+typedef void (^RSGetVodUrlCallBack) (NSString*, NSError*);
 
 @interface RestService : NSObject
 
 +(void)SendLinkingRequest:(NSString *)baseUrl withDeviceId:(NSString *)deviceId andDeviceTypeId:(NSString *)deviceTypeId andCallback:(NSObject *)callbackObject usingSelector:(SEL)callbackSelector;
 
 +(void)SendLinkingRequest:(NSString *)baseUrl withDeviceId:(NSString *)deviceId andDeviceTypeId:(NSString *)deviceTypeId usingCallback:(RSLinkingCallBack)callback;
+
++(void)RequestGetVODUrl:(NSString *)baseUrl ofVideo:(NSString *)videoId withDeviceId:(NSString *)deviceId andDeviceTypeId:(NSString *)deviceTypeId usingCallback:(RSGetVodUrlCallBack)callback;
 
 @end
--- MyTV.IOS.Framework/MyTV.IOS/RestService.m
+++ MyTV.IOS.Framework/MyTV.IOS/RestService.m
@@ -119,4 +119,52 @@
     
 }
 
++(void)RequestGetVODUrl:(NSString *)baseUrl ofVideo:(NSString *)videoId withDeviceId:(NSString *)deviceId andDeviceTypeId:(NSString *)deviceTypeId usingCallback:(RSGetVodUrlCallBack)callback {
+    
+    NSString* linkingRequestUrl = [baseUrl stringByAppendingString:[NSString stringWithFormat:@"action=watchvod&deviceid=%@&devicetypeid=%@&episodeid=%@", deviceId, deviceTypeId, videoId]];
+    
+    [DataFetcher Get:linkingRequestUrl usingCallback:^(NSData *data, NSError *error) {
+        DLog(@"Processing Data inside Code Block");
+        if (error != NULL) {
+            callback(NULL, error);
+        }
+        else {
+            if(data == NULL) {
+                callback(NULL, NULL);
+            }
+            else {
+                NSError *error;
+                //GDataXMLDocument *document = [[GDataXMLDocument alloc] initWithData:data options:0 error:&error];
+                TBXML *document = [TBXML newTBXMLWithXMLData:data error:&error];
+                if(error != NULL) {
+                    callback(NULL, error);
+                }
+                else {
+                    TBXMLElement *root = document.rootXMLElement;
+                    NSString *status = [TBXML textForElement:[TBXML childElementNamed:@"status" parentElement:root]];
+                    if ([status compare:@"failure"] == NSOrderedSame) {
+                        error = [NSError errorWithDomain:NSPOSIXErrorDomain code:[@"1" integerValue] userInfo:@{NSLocalizedDescriptionKey: @"Webservice Failure"}];
+                        callback(NULL, error);
+                    }
+                    else
+                    {
+                        TBXMLElement *root = document.rootXMLElement;
+                        TBXMLElement *url = [TBXML childElementNamed:@"URL" parentElement:root];
+                        if (url == NULL) {
+                            callback(NULL, NULL);
+                        }
+                        else {
+                            NSString* playurl =  [TBXML textForElement:url];
+                            callback(playurl, NULL);
+                        }
+                        
+                    }
+                    
+                }
+            }
+        }
+    }];
+    
+}
+
 @end
