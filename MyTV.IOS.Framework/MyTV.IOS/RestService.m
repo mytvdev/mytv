91c000407971a22658c5e7b0f11473d8f00f21a9
Omar Salloum <proneoas@gmail.com>
2012-08-28 17:23:55 +0300

    RestService check for status
    Added a check to see if the status element exists, if it does not, we
    pass to the callback NULL parameters

--- MyTV.IOS.Framework/MyTV.IOS/RestService.m
+++ MyTV.IOS.Framework/MyTV.IOS/RestService.m
@@ -23,41 +23,47 @@
         }
         else {
             NSError *error;
-            //GDataXMLDocument *document = [[GDataXMLDocument alloc] initWithData:data options:0 error:&error];
             TBXML *document = [TBXML newTBXMLWithXMLData:data error:&error];
             if(error != NULL) {
                 [callbackObject performSelector:callbackSelector withObject:data withObject:error];
             }
             else {
                 TBXMLElement *root = document.rootXMLElement;
-                NSString *status = [TBXML textForElement:[TBXML childElementNamed:@"status" parentElement:root]];
-                if ([status compare:@"failure"] == NSOrderedSame) {
-                    error = [NSError errorWithDomain:NSPOSIXErrorDomain code:[@"1" integerValue] userInfo:@{NSLocalizedDescriptionKey: @"Webservice Failure"}];
-                    
-                    [callbackObject performSelector:callbackSelector withObject:NULL withObject:error];
+                TBXMLElement *statusEl = [TBXML childElementNamed:@"status" parentElement:root];
+                if(statusEl == NULL) {
+                    DLog(@"No status element found in xml. Passing NULL parameters to callback");
+                    [callbackObject performSelector:callbackSelector withObject:NULL withObject:NULL];
                 }
-                else
-                {
-                    Linking *object = [Linking new];
-                    TBXMLElement *root = document.rootXMLElement;
-                    TBXMLElement *customer = [TBXML childElementNamed:@"customer" parentElement:root];
-                    if (customer == NULL) {
-                        [callbackObject performSelector:callbackSelector withObject:NULL withObject:NULL];
+                else {
+                    NSString *status = [TBXML textForElement:statusEl];
+                    if ([status compare:@"failure"] == NSOrderedSame) {
+                        error = [NSError errorWithDomain:NSPOSIXErrorDomain code:[@"1" integerValue] userInfo:@{NSLocalizedDescriptionKey: @"Webservice Failure"}];
+                        
+                        [callbackObject performSelector:callbackSelector withObject:NULL withObject:error];
                     }
-                    else {
-                        TBXMLElement *pin = [TBXML childElementNamed:@"PinCode" parentElement:customer];
-                        TBXMLElement *customerid = [TBXML childElementNamed:@"Id" parentElement:customer];
-                        TBXMLElement *billingid = [TBXML childElementNamed:@"BillingId" parentElement:customer];
-                        if(pin != NULL)
-                            object.PinCode = [ScanHelper getIntFromNSString:[TBXML textForElement:pin]];
-                        if(customerid != NULL)
-                            object.CustomerId = [ScanHelper getIntFromNSString:[TBXML textForElement:customerid]];
-                        if(billingid != NULL)
-                            object.BillingId = [ScanHelper getIntFromNSString:[TBXML textForElement:billingid]];
-                        [callbackObject performSelector:callbackSelector withObject:object withObject:NULL];
+                    else
+                    {
+                        Linking *object = [Linking new];
+                        TBXMLElement *root = document.rootXMLElement;
+                        TBXMLElement *customer = [TBXML childElementNamed:@"customer" parentElement:root];
+                        if (customer == NULL) {
+                            [callbackObject performSelector:callbackSelector withObject:NULL withObject:NULL];
+                        }
+                        else {
+                            TBXMLElement *pin = [TBXML childElementNamed:@"PinCode" parentElement:customer];
+                            TBXMLElement *customerid = [TBXML childElementNamed:@"Id" parentElement:customer];
+                            TBXMLElement *billingid = [TBXML childElementNamed:@"BillingId" parentElement:customer];
+                            if(pin != NULL)
+                                object.PinCode = [ScanHelper getIntFromNSString:[TBXML textForElement:pin]];
+                            if(customerid != NULL)
+                                object.CustomerId = [ScanHelper getIntFromNSString:[TBXML textForElement:customerid]];
+                            if(billingid != NULL)
+                                object.BillingId = [ScanHelper getIntFromNSString:[TBXML textForElement:billingid]];
+                            [callbackObject performSelector:callbackSelector withObject:object withObject:NULL];
+                            
+                        }
                         
                     }
-                    
                 }
                 
             }
@@ -77,40 +83,46 @@
         }
         else {
             NSError *error;
-            //GDataXMLDocument *document = [[GDataXMLDocument alloc] initWithData:data options:0 error:&error];
             TBXML *document = [TBXML newTBXMLWithXMLData:data error:&error];
             if(error != NULL) {
                 callback(NULL, error);
             }
             else {
                 TBXMLElement *root = document.rootXMLElement;
-                NSString *status = [TBXML textForElement:[TBXML childElementNamed:@"status" parentElement:root]];
-                if ([status compare:@"failure"] == NSOrderedSame) {
-                    error = [NSError errorWithDomain:NSPOSIXErrorDomain code:[@"1" integerValue] userInfo:@{NSLocalizedDescriptionKey: @"Webservice Failure"}];
-                    callback(NULL, error);
+                TBXMLElement *statusEl = [TBXML childElementNamed:@"status" parentElement:root];
+                if(statusEl == NULL) {
+                    DLog(@"No status element found in xml. Passing NULL parameters to callback");
+                    callback(NULL, NULL);
                 }
-                else
-                {
-                    Linking *object = [Linking new];
-                    TBXMLElement *root = document.rootXMLElement;
-                    TBXMLElement *customer = [TBXML childElementNamed:@"customer" parentElement:root];
-                    if (customer == NULL) {
-                        callback(NULL, NULL);
+                else {
+                    NSString *status = [TBXML textForElement:statusEl];
+                    if ([status compare:@"failure"] == NSOrderedSame) {
+                        error = [NSError errorWithDomain:NSPOSIXErrorDomain code:[@"1" integerValue] userInfo:@{NSLocalizedDescriptionKey: @"Webservice Failure"}];
+                        callback(NULL, error);
                     }
-                    else {
-                        TBXMLElement *pin = [TBXML childElementNamed:@"PinCode" parentElement:customer];
-                        TBXMLElement *customerid = [TBXML childElementNamed:@"Id" parentElement:customer];
-                        TBXMLElement *billingid = [TBXML childElementNamed:@"BillingId" parentElement:customer];
-                        if(pin != NULL)
-                            object.PinCode = [ScanHelper getIntFromNSString:[TBXML textForElement:pin]];
-                        if(customerid != NULL)
-                            object.CustomerId = [ScanHelper getIntFromNSString:[TBXML textForElement:customerid]];
-                        if(billingid != NULL)
-                            object.BillingId = [ScanHelper getIntFromNSString:[TBXML textForElement:billingid]];
-                        callback(object, NULL);
+                    else
+                    {
+                        Linking *object = [Linking new];
+                        TBXMLElement *root = document.rootXMLElement;
+                        TBXMLElement *customer = [TBXML childElementNamed:@"customer" parentElement:root];
+                        if (customer == NULL) {
+                            callback(NULL, NULL);
+                        }
+                        else {
+                            TBXMLElement *pin = [TBXML childElementNamed:@"PinCode" parentElement:customer];
+                            TBXMLElement *customerid = [TBXML childElementNamed:@"Id" parentElement:customer];
+                            TBXMLElement *billingid = [TBXML childElementNamed:@"BillingId" parentElement:customer];
+                            if(pin != NULL)
+                                object.PinCode = [ScanHelper getIntFromNSString:[TBXML textForElement:pin]];
+                            if(customerid != NULL)
+                                object.CustomerId = [ScanHelper getIntFromNSString:[TBXML textForElement:customerid]];
+                            if(billingid != NULL)
+                                object.BillingId = [ScanHelper getIntFromNSString:[TBXML textForElement:billingid]];
+                            callback(object, NULL);
+                            
+                        }
                         
                     }
-                    
                 }
                 
             }
@@ -134,32 +146,38 @@
             }
             else {
                 NSError *error;
-                //GDataXMLDocument *document = [[GDataXMLDocument alloc] initWithData:data options:0 error:&error];
                 TBXML *document = [TBXML newTBXMLWithXMLData:data error:&error];
                 if(error != NULL) {
                     callback(NULL, error);
                 }
                 else {
                     TBXMLElement *root = document.rootXMLElement;
-                    NSString *status = [TBXML textForElement:[TBXML childElementNamed:@"status" parentElement:root]];
-                    if ([status compare:@"failure"] == NSOrderedSame) {
-                        error = [NSError errorWithDomain:NSPOSIXErrorDomain code:[@"1" integerValue] userInfo:@{NSLocalizedDescriptionKey: @"Webservice Failure"}];
-                        callback(NULL, error);
+                    TBXMLElement *statusEl = [TBXML childElementNamed:@"status" parentElement:root];
+                    if(statusEl == NULL) {
+                        DLog(@"No status element found in xml. Passing NULL parameters to callback");
+                        callback(NULL, NULL);
                     }
-                    else
-                    {
-                        TBXMLElement *root = document.rootXMLElement;
-                        TBXMLElement *url = [TBXML childElementNamed:@"URL" parentElement:root];
-                        if (url == NULL) {
-                            callback(NULL, NULL);
+                    else {
+                        NSString *status = [TBXML textForElement:statusEl];
+                        if ([status compare:@"failure"] == NSOrderedSame) {
+                            error = [NSError errorWithDomain:NSPOSIXErrorDomain code:[@"1" integerValue] userInfo:@{NSLocalizedDescriptionKey: @"Webservice Failure"}];
+                            callback(NULL, error);
                         }
-                        else {
-                            NSString* playurl =  [TBXML textForElement:url];
-                            callback(playurl, NULL);
+                        else
+                        {
+                            TBXMLElement *root = document.rootXMLElement;
+                            TBXMLElement *url = [TBXML childElementNamed:@"URL" parentElement:root];
+                            if (url == NULL) {
+                                callback(NULL, NULL);
+                            }
+                            else {
+                                NSString* playurl =  [TBXML textForElement:url];
+                                callback(playurl, NULL);
+                            }
+                            
                         }
                         
                     }
-                    
                 }
             }
         }
